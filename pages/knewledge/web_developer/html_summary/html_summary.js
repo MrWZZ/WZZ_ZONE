b44aca1463cb8d178411805d5a29467f8e92e68f
wzz <872173938@qq.com>
2018-01-03 16:06:43 +0800

    自动生成链接

---
+++ pages/knewledge/web_developer/html_summary/html_summary.js
@@ -0,0 +1,46 @@
+
+//页面数组
+var pages = [
+  { title:"创建文档", url:"pages/creating_html.html" },
+  { title:"标记文字", url:"pages/marking_word.html" },
+  { title:"组织内容", url:"pages/organizing_content.html" },
+  { title:"文档分节", url:"pages/document_segmentation.html" },
+  { title:"表格元素", url:"pages/table_element.html" }
+];
+
+//导航链接生成
+function CreateLink()
+{
+  var navContent = document.querySelector(".w_nav");
+  for(var i in pages)
+  {
+    var a = document.createElement("a");
+    a.setAttribute("href",pages[i].url);
+    a.setAttribute("target","page_content");
+    a.setAttribute("class","w_blur");
+    a.text = pages[i].title;
+    navContent.appendChild(a);
+  }
+}
+
+//iframe高度自适应
+function setIframeHeight()
+{
+  var frame = document.getElementById("page_content");
+  var frameContent = frame.contentWindow.document;
+  var frameHeight = frameContent.body.scrollHeight || frameContent.documentElement.scrollHeight;
+  frame.height = frameHeight;
+  frame.onload = function()
+  {
+    frameContent = frame.contentWindow.document;
+    frameHeight = frameContent.body.scrollHeight || frameContent.documentElement.scrollHeight;
+    frame.height = frameHeight;
+  }
+}
+
+//页面初始化
+(function PageInitial()
+{
+  CreateLink();
+  setIframeHeight();
+})();
