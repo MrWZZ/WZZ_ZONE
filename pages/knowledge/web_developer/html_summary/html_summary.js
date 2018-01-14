89f49e96d7daf0aa0131e9a69d0597f7cde3eba6
WZZ <872173938@qq.com>
2018-01-14 14:51:52 +0800

    页面调整

--- pages/knowledge/web_developer/html_summary/html_summary.js
+++ pages/knowledge/web_developer/html_summary/html_summary.js
@@ -3,11 +3,11 @@ var pageContent;          //子页面
 
 //页面数组
 var pages = [
-  { title:"创建文档", url:"pages/creating_html.html" },
-  { title:"标记文字", url:"pages/marking_word.html" },
-  { title:"组织内容", url:"pages/organizing_content.html" },
-  { title:"文档分节", url:"pages/document_segmentation.html" },
-  { title:"表格元素", url:"pages/table_element.html" }
+  { title:"创建文档", url:"pages/1_creating_html.html" },
+  { title:"标记文字", url:"pages/2_marking_word.html" },
+  { title:"组织内容", url:"pages/3_organizing_content.html" },
+  { title:"文档分节", url:"pages/4_document_segmentation.html" },
+  { title:"表格元素", url:"pages/5_table_element.html" }
 ];
 
 //导航链接生成
