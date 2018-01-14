876564977a2a73a48baa88fbdb24d485e11bca01
WZZ <872173938@qq.com>
2018-01-14 17:24:13 +0800

    脚本大修改

---
+++ pages/knowledge/web_developer/html_summary/pages.js
@@ -0,0 +1,8 @@
+//页面数组
+var pages = [
+  { title:"创建文档", url:"pages/1_creating_html.html" },
+  { title:"标记文字", url:"pages/2_marking_word.html" },
+  { title:"组织内容", url:"pages/3_organizing_content.html" },
+  { title:"文档分节", url:"pages/4_document_segmentation.html" },
+  { title:"表格元素", url:"pages/5_table_element.html" }
+];
