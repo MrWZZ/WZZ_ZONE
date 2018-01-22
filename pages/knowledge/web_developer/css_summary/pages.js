d91d13a56229132884abf98c893ed3cbe4f834a8
WZZ <872173938@qq.com>
2018-01-22 20:13:59 +0800

    0

--- pages/knowledge/web_developer/css_summary/pages.js
+++ pages/knowledge/web_developer/css_summary/pages.js
@@ -2,9 +2,6 @@
 var pages = [
   { title:"概述", url:"pages/1.html" },
   { title:"属性简明介绍", url:"pages/2.html" },
-  { title:"组织内容", url:"pages/3_organizing_content.html" },
-  { title:"文档分节", url:"pages/4_document_segmentation.html" },
-  { title:"表格元素", url:"pages/5_table_element.html" },
-  { title:"表单", url:"pages/6_form.html" },
-  { title:"嵌入元素", url:"pages/7_inset.html" }
+  { title:"设置文本样式", url:"pages/3.html" },
+  { title:"过渡、动画、变换", url:"pages/4.html" }
 ];
