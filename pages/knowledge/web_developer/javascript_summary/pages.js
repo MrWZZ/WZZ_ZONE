36926c62237c25569bbf21673eb3108a342c540a
WZZ <872173938@qq.com>
2018-03-10 17:39:39 +0800

    JS模块完成

--- pages/knowledge/web_developer/javascript_summary/pages.js
+++ pages/knowledge/web_developer/javascript_summary/pages.js
@@ -1,5 +1,7 @@
 //页面数组
 var pages = [
-  { title:"概述", url:"pages/1.html" },
-  { title:"理解DOM",url:"pages/2.html"}
+  { title:"属性预览",url:"pages/1.html"},
+  { title:"基础入门", url:"pages/2.html" },
+  { title:"对象", url:"pages/3.html" },
+  { title:"正则表达式", url:"pages/4.html" },
 ];
