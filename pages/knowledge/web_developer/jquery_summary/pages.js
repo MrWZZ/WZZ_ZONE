72dad1333a70c143f87d630fd074004c1ca9da6f
WZZ <872173938@qq.com>
2018-03-13 14:19:28 +0800

    jQuery完成

--- pages/knowledge/web_developer/jquery_summary/pages.js
+++ pages/knowledge/web_developer/jquery_summary/pages.js
@@ -1,4 +1,11 @@
 //页面数组
 var pages = [
-  { title:"概述", url:"pages/1.html" }
+  { title:"基础入门", url:"pages/1.html" },
+  { title:"jQuery选择器", url:"pages/2.html" },
+  { title:"操作jQuery集合", url:"pages/3.html" },
+  { title:"jQuery中的DOM操作", url:"pages/4.html" },
+  { title:"jQuery中的HTML操作", url:"pages/5.html" },
+  { title:"jQuery中的事件处理", url:"pages/6.html" },
+  { title:"jQuery与Ajax", url:"pages/7.html" },
+  { title:"应用jQuery动画效果", url:"pages/8.html" },
 ];
