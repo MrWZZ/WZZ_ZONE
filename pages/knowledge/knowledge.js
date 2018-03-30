309d46973253034d19df36f1b1b33dda5f2eff46
WZZ <872173938@qq.com>
2018-03-30 09:27:21 +0800

    c#前10章+页面调整

--- pages/knowledge/knowledge.js
+++ pages/knowledge/knowledge.js
@@ -36,13 +36,14 @@ var knowledgeList = [
    slots:[
     {title:"Unity3D",url:"#"},
     {title:"Atom",url:"software/atom/atom.html"},
+    {title:"Visual Studio",url:"software/vs/vs.html"},
     {title:"Git",url:"software/git/git.html"}
   ]},
   //语言
   {type : "语言",
    slots:[
     {title:"C++",url:"#"},
-    {title:"C#",url:"#"},
+    {title:"C#",url:"language/csharp/csharp.html"},
     {title:"UML",url:"language/uml/uml.html"},
     {title:"设计模式",url:"language/design_pattern/design_pattern.html"},
     {title:"数据结构与算法",url:"language/structures_and_algorithms/structures_and_algorithms.html"}
---
+++ pages/knowledge/language/csharp/pages.js
@@ -0,0 +1,14 @@
+//页面数组
+var pages = [
+  { title:"C#核心编成结构I", url:"pages/1.html" },
+  { title:"C#核心编成结构II", url:"pages/2.html" },
+  { title:"C#面向对象编程", url:"pages/3.html" },
+  { title:"结构化异常处理", url:"pages/4.html" },
+  { title:"接口", url:"pages/5.html" },
+  { title:"泛型", url:"pages/6.html" },
+  { title:"委托、事件、Lambda", url:"pages/7.html" },
+  { title:"高级语言特性", url:"pages/8.html" },
+  { title:"反射", url:"pages/9.html" },
+  { title:"多线程、并行和异步", url:"pages/10.html" },
+  { title:"文件输入输出和对象序列化", url:"pages/10.html" },
+];
