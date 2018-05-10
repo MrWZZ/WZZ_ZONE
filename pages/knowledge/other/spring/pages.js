000272f51ced5df2cdf864217ecd27a3e048f259
WZZ <872173938@qq.com>
2018-05-10 23:34:06 +0800

    .h1样式修改

--- pages/knowledge/other/spring/pages.js
+++ pages/knowledge/other/spring/pages.js
@@ -3,5 +3,5 @@ var pages = [
   { title:"IOC", url:"pages/1.html" },
   { title:"AOP", url:"pages/2.html" },
   { title:"JDBC和事务", url:"pages/3.html" },
-  { title:"", url:"pages/4.html" },
+  { title:"SpringMVC", url:"pages/4.html" },
 ];
