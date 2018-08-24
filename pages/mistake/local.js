4ddc1c43e297e4f6a44f23ceb7a7f46651522bbc
wzz <872173938@qq.com>
2018-08-24 21:00:27 +0800

    egret模块

--- pages/mistake/local.js
+++ pages/mistake/local.js
@@ -3,6 +3,7 @@ var pages = [
   { title:"JavaScript", url:"pages/java_script.html" },
   { title:"Atom", url:"pages/atom.html" },
   { title:"CSS", url:"pages/css.html" },
+  { title:"egret", url:"pages/egret.html" },
   { title:"其他", url:"pages/other.html" },
 ];
 var info = {
