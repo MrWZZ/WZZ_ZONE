5d3058cc57987e36ae007d9b6474adc0db1c17bb
wzz <872173938@qq.com>
2018-11-06 18:51:44 +0800

    0

--- pages/knowledge/other/web_gl/test/main.js
+++ pages/knowledge/other/web_gl/test/main.js
@@ -1,36 +1,37 @@
 /**
- * 平移、旋转、缩放
+ * 纹理
+ * 1、准备好映射到几何图形上的纹理图像
+ * 2、为几何图形配置纹理映射方式
+ * 3、加载纹理图像、对其进行一些配置，在webgl中使用
+ * 4、在片元着色器中将形影相应的纹素从纹理中抽取处理来，并将纹素的颜色赋给片元
  */
 var Main = /** @class */ (function () {
     function Main() {
     }
     Main.start = function () {
         var gl = Shader.getGl("canvas");
-        this.gl = gl;
         var vSource = 'attribute vec4 a_Position;\n' +
-            'uniform mat4 u_xformMatrix;\n' +
+            'attribute vec2 a_TexCoord;\n' +
+            'varying vec2 v_TexCoord;\n' +
             'void main() {\n' +
-            '  gl_Position = u_xformMatrix * a_Position;\n' +
+            '  gl_Position = a_Position;\n' +
+            '  v_TexCoord = a_TexCoord;\n' +
             '}\n';
         var fSource = 'precision mediump float;\n' +
-            'uniform vec4 u_FragColor;\n' +
+            'varying vec2 v_TexCoord;\n' +
+            'uniform sampler2D u_Sampler;\n' +
             'void main() {\n' +
-            '  gl_FragColor = u_FragColor;\n' +
+            '  gl_FragColor = texture2D(u_Sampler,v_TexCoord);\n' +
             '}\n';
         var program = Shader.createProgram(gl, vSource, fSource);
         gl.useProgram(program);
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        //获取attribute变量
-        var a_Position = Shader.getAttribLocation(gl, program, "a_Position");
-        //赋值（使用缓冲区对象后不能使用这种方式赋值）
-        // gl.vertexAttrib2f(a_Position,0.5,0.5);
-        //获取uniform变量
-        var u_FragColor = Shader.getUniformLocation(gl, program, "u_FragColor");
-        //赋值
-        gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
-        //缓冲数据
+        //缓冲数据:顶点坐标、纹理坐标
         var vArray = new Float32Array([
-            0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+            -0.5, 0.5, 0.0, 1.0,
+            -0.5, -0.5, 0.0, 0.0,
+            0.5, 0.5, 1.0, 1.0,
+            0.5, -0.5, 1.0, 0.0
         ]);
         //创建缓冲区对象
         var vBuffer = Shader.createBuffer(gl);
@@ -38,71 +39,52 @@ var Main = /** @class */ (function () {
         gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
         //将数据写入缓冲区对象
         gl.bufferData(gl.ARRAY_BUFFER, vArray, gl.STATIC_DRAW);
+        //获取分量长度
+        var FSIZE = vArray.BYTES_PER_ELEMENT;
+        //获取attribute变量
+        var a_Position = Shader.getAttribLocation(gl, program, "a_Position");
         //将缓冲区对象分配给一个attribute变量
-        gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+        gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
         //开启attribute变量
         gl.enableVertexAttribArray(a_Position);
-        //变换矩阵获取
-        var u_xformMatrix = Shader.getUniformLocation(gl, program, "u_xformMatrix");
-        this.u_xformMatrix = u_xformMatrix;
-        //旋转角度
-        this.angle = 90;
-        var radian = this.angle * Math.PI / 180;
-        var cosB = Math.cos(radian);
-        var sinB = Math.sin(radian);
-        //旋转矩阵（列主序）
-        var xformMatrix = new Float32Array([
-            cosB, sinB, 0.0, 0.0,
-            -sinB, cosB, 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0,
-            0.0, 0.0, 0.0, 1.0
-        ]);
-        //平移矩阵
-        // let Tx = 0.5,Ty = 0.5,Tz = 0.5;
-        // let xformMatrix = new Float32Array([
-        //     1.0,0.0,0.0,0.0,
-        //     0.0,1.0,0.0,0.0,
-        //     0.0,0.0,1.0,0.0,
-        //     Tx,Ty,Tz,1.0
-        // ]);
-        //缩放矩阵
-        // let Sx = 1.0,Sy = 1.5,Sz = 1.0;
-        // let xformMatrix = new Float32Array([
-        //     Sx,0.0,0.0,0.0,
-        //     0.0,Sy,0.0,0.0,
-        //     0.0,0.0,Sz,0.0,
-        //     0.0,0.0,0.0,1.0
-        // ]);
-        //将许旋转矩阵传输给顶点着色器
-        gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);
+        //获取attribute变量
+        var a_TexCoord = Shader.getAttribLocation(gl, program, "a_TexCoord");
+        //将缓冲区对象分配给一个attribute变量
+        gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+        //开启attribute变量
+        gl.enableVertexAttribArray(a_TexCoord);
+        //加载纹理
+        initTextures(gl, program);
         //绘制
         gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.drawArrays(gl.TRIANGLES, 0, 4);
     };
-    Main.update = function () {
-        this.angle++;
-        var radian = this.angle * Math.PI / 180;
-        var cosB = Math.cos(radian);
-        var sinB = Math.sin(radian);
-        //旋转矩阵（列主序）
-        var xformMatrix = new Float32Array([
-            cosB, sinB, 0.0, 0.0,
-            -sinB, cosB, 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0,
-            0.0, 0.0, 0.0, 1.0
-        ]);
-        this.gl.uniformMatrix4fv(this.u_xformMatrix, false, xformMatrix);
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
-        this.gl.drawArrays(this.gl.TRIANGLES, 0, 3);
+    Main.initTextures = function ($gl, $program) {
     };
-    Main.angle = 90;
     return Main;
 }());
 Main.start();
-
-var update = function () {
-    Main.update();
-    requestAnimationFrame(update);   
+function initTextures($gl, $program) {
+    var texture = $gl.createTexture();
+    var u_Sampler = Shader.getUniformLocation($gl, $program, "u_Sampler");
+    //加载图片
+    var image = new Image();
+    image.onload = function () {
+        onLoadImage($gl, texture, u_Sampler, image);
+    };
+    image.src = "sky.JPG";
 }
-
-update();
+function onLoadImage($gl, $texture, $u_Sampler, $image) {
+    //对纹理图像进行y轴反转
+    $gl.pixelStorei($gl.UNPACK_FLIP_Y_WEBGL, 1);
+    //开启0号纹理单元
+    $gl.activeTexture($gl.TEXTURE0);
+    //向target绑定纹理对象
+    $gl.bindTexture($gl.TEXTURE_2D, $texture);
+    //配置纹理参数
+    $gl.texParameteri($gl.TEXTURE_2D, $gl.TEXTURE_MIN_FILTER, $gl.LINEAR);
+    //配置纹理图像
+    $gl.texImage2D($gl.TEXTURE_2D, 0, $gl.RGB, $gl.RGB, $gl.UNSIGNED_BYTE, $image);
+    //将0号纹理传给着色器参数
+    $gl.uniform1i($u_Sampler, 0);
+}
--- pages/knowledge/other/web_gl/test/ts/Main.ts
+++ pages/knowledge/other/web_gl/test/ts/Main.ts
@@ -1,5 +1,9 @@
 /**
- * 平移、旋转、缩放
+ * 纹理
+ * 1、准备好映射到几何图形上的纹理图像
+ * 2、为几何图形配置纹理映射方式
+ * 3、加载纹理图像、对其进行一些配置，在webgl中使用
+ * 4、在片元着色器中将形影相应的纹素从纹理中抽取处理来，并将纹素的颜色赋给片元
  */
 
 class Main {
@@ -9,83 +13,87 @@ class Main {
         let gl = Shader.getGl("canvas");
         let vSource =
             'attribute vec4 a_Position;\n' +
-            'uniform mat4 u_xformMatrix;\n' +
+            'attribute vec2 a_TexCoord;\n' +
+            'varying vec2 v_TexCoord;\n' +
             'void main() {\n' +
-            '  gl_Position = u_xformMatrix * a_Position;\n' +
+            '  gl_Position = a_Position;\n' +
+            '  v_TexCoord = a_TexCoord;\n' +
             '}\n';
 
         let fSource =
             'precision mediump float;\n' +
-            'uniform vec4 u_FragColor;\n' +
+            'varying vec2 v_TexCoord;\n' +
+            'uniform sampler2D u_Sampler;\n' +
             'void main() {\n' +
-            '  gl_FragColor = u_FragColor;\n' + 
+            '  gl_FragColor = texture2D(u_Sampler,v_TexCoord);\n' +
             '}\n';
 
-        let program = Shader.createProgram(gl,vSource,fSource);
+        let program = Shader.createProgram(gl, vSource, fSource);
         gl.useProgram(program);
-        gl.clearColor(0.0,0.0,0.0,1.0);
-        //获取attribute变量
-        let a_Position = Shader.getAttribLocation(gl,program,"a_Position");
-        //赋值（使用缓冲区对象后不能使用这种方式赋值）
-        // gl.vertexAttrib2f(a_Position,0.5,0.5);
-        //获取uniform变量
-        let u_FragColor = Shader.getUniformLocation(gl,program,"u_FragColor");
-        //赋值
-        gl.uniform4f(u_FragColor,1.0,0.0,0.0,1.0);
-        
-        //缓冲数据
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+        //缓冲数据:顶点坐标、纹理坐标
         let vArray = new Float32Array([
-            0.0,0.5,-0.5,-0.5,0.5,-0.5
+            -0.5, 0.5, 0.0, 1.0,
+            -0.5, -0.5, 0.0, 0.0,
+            0.5, 0.5, 1.0, 1.0,
+            0.5, -0.5, 1.0, 0.0
         ]);
         //创建缓冲区对象
         let vBuffer = Shader.createBuffer(gl);
         //绑定缓冲区对象
-        gl.bindBuffer(gl.ARRAY_BUFFER,vBuffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
         //将数据写入缓冲区对象
-        gl.bufferData(gl.ARRAY_BUFFER,vArray,gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, vArray, gl.STATIC_DRAW);
+        //获取分量长度
+        let FSIZE = vArray.BYTES_PER_ELEMENT;
+        //获取attribute变量
+        let a_Position = Shader.getAttribLocation(gl, program, "a_Position");
         //将缓冲区对象分配给一个attribute变量
-        gl.vertexAttribPointer(a_Position,2,gl.FLOAT,false,0,0);
+        gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
         //开启attribute变量
         gl.enableVertexAttribArray(a_Position);
 
-        //变换矩阵获取
-        let u_xformMatrix = Shader.getUniformLocation(gl,program,"u_xformMatrix");
-        //旋转角度
-        let angle = 90;
-        let radian = angle * Math.PI/180;
-        let cosB = Math.cos(radian);
-        let sinB = Math.sin(radian);
-        //旋转矩阵（列主序）
-        let xformMatrix = new Float32Array([
-            cosB,sinB,0.0,0.0,
-            -sinB,cosB,0.0,0.0,
-            0.0,0.0,1.0,0.0,
-            0.0,0.0,0.0,1.0
-        ]);
-        //平移矩阵
-        // let Tx = 0.5,Ty = 0.5,Tz = 0.5;
-        // let xformMatrix = new Float32Array([
-        //     1.0,0.0,0.0,0.0,
-        //     0.0,1.0,0.0,0.0,
-        //     0.0,0.0,1.0,0.0,
-        //     Tx,Ty,Tz,1.0
-        // ]);
-        //缩放矩阵
-        // let Sx = 1.0,Sy = 1.5,Sz = 1.0;
-        // let xformMatrix = new Float32Array([
-        //     Sx,0.0,0.0,0.0,
-        //     0.0,Sy,0.0,0.0,
-        //     0.0,0.0,Sz,0.0,
-        //     0.0,0.0,0.0,1.0
-        // ]);
+        //获取attribute变量
+        let a_TexCoord = Shader.getAttribLocation(gl, program, "a_TexCoord");
+        //将缓冲区对象分配给一个attribute变量
+        gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+        //开启attribute变量
+        gl.enableVertexAttribArray(a_TexCoord);
 
-        //将许旋转矩阵传输给顶点着色器
-        gl.uniformMatrix4fv(u_xformMatrix,false,xformMatrix);
+        let texture = gl.createTexture();
+        let u_Sampler = Shader.getUniformLocation(gl, program, "u_Sampler");
 
+        //加载纹理
+        initTextures(gl, program);
         //绘制
         gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES,0,3);
+        gl.drawArrays(gl.TRIANGLES, 0, 4);
     }
 }
 
 Main.start();
+
+function initTextures($gl, $program) {
+    //加载图片
+    let image = new Image();
+    image.onload = function () {
+        onLoadImage($gl,texture,u_Sampler,image);
+    }
+    image.src = "sky.jpg";
+}
+
+function onLoadImage($gl, $texture, $u_Sampler, $image): void {
+    //对纹理图像进行y轴反转
+    $gl.pixelStorei($gl.UNPACK_FLIP_Y_WEBGL, 1);
+    //开启0号纹理单元
+    $gl.activeTexture($gl.TEXTURE0);
+    //向target绑定纹理对象
+    $gl.bindTexture($gl.TEXTURE_2D, $texture);
+    //配置纹理参数
+    $gl.texParameteri($gl.TEXTURE_2D, $gl.TEXTURE_MIN_FILTER, $gl.LINEAR);
+    //配置纹理图像
+    $gl.texImage2D($gl.TEXTURE_2D, 0, $gl.RGB, $gl.RGB, $gl.UNSIGNED_BYTE, $image);
+    //将0号纹理传给着色器参数
+    $gl.uniform1i($u_Sampler, 0);
+}
