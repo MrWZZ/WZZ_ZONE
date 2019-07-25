83e098153cd55bc3a47fce94b2e9adc6151ba186
wzz <872173938@qq.com>
2019-07-25 19:58:36 +0800

    jdw

--- pages/method/local.js
+++ pages/method/local.js
@@ -1,10 +1,11 @@
 //页面数组
 var pages = [
-  { title:"tool", url:"pages/tool.html" },
-  { title:"js", url:"pages/js.html" },
+    { title: "tool", url: "pages/tool.html" },
+    { title: "jdw", url: "pages/jdw.html" },
+    { title: "js", url: "pages/js.html" },
 ];
 var info = {
-  title : "方法库",
-  description : ""
+    title: "方法库",
+    description: ""
 }
-Controller.loadView(mainData.view.summary,Controller.pageInit,[pages,info]);
+Controller.loadView(mainData.view.summary, Controller.pageInit, [pages, info]);
